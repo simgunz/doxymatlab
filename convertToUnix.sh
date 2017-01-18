ae8e7112980771bad8b70ad0f3b5553a51ebe1c7
Simone Gaiarin <simgunz@gmail.com>
2017-01-18 14:57:01 +0100

    Replace .bat with .pl

--- convertToUnix.sh
+++ convertToUnix.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 sed -i 's/perl.exe/perl/g' *.pl  # Remove .exe extension
 sed -i 's/\r//g' *.pl            # Remove windows carriage return from perl scripts
+sed -i 's/m2cpp.bat/.\/m2cpp.pl/g'  Doxyfile
+sed -i 's/m2srcm.bat/.\/m2srcm.pl/g'  Doxyfile
