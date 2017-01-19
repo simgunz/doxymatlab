c713d9f0aea49faf4fd2ab32789bf1937a2e85fd
Simone Gaiarin <simgunz@gmail.com>
2017-01-19 13:27:22 +0100

    Make perl script executable on linux

--- convertToUnix.sh
+++ convertToUnix.sh
@@ -3,3 +3,5 @@ sed -i 's/perl.exe/perl/g' *.pl  # Remove .exe extension
 sed -i 's/\r//g' *.pl            # Remove windows carriage return from perl scripts
 sed -i 's/m2cpp.bat/.\/m2cpp.pl/g'  Doxyfile
 sed -i 's/m2srcm.bat/.\/m2srcm.pl/g'  Doxyfile
+chmod +x m2cpp.pl
+chmod +x m2srcm.pl
